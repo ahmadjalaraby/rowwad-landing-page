d9bf977b5f055392f48da1579f59110514847823
ayanogin <barahuhu@gmail.com>
2022-10-07 21:55:30 -0700

    'baraa'

--- js/script.js
+++ js/script.js
@@ -36,3 +36,81 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+(function ($) {
+  "use strict";
+  /*==================================================================
+      [ Daterangepicker ]*/
+  try {
+    $(".js-datepicker").daterangepicker({
+      singleDatePicker: true,
+      showDropdowns: true,
+      autoUpdateInput: false,
+      locale: {
+        format: "DD/MM/YYYY",
+      },
+    });
+
+    var myCalendar = $(".js-datepicker");
+    var isClick = 0;
+
+    $(window).on("click", function () {
+      isClick = 0;
+    });
+
+    $(myCalendar).on("apply.daterangepicker", function (ev, picker) {
+      isClick = 0;
+      $(this).val(picker.startDate.format("DD/MM/YYYY"));
+    });
+
+    $(".js-btn-calendar").on("click", function (e) {
+      e.stopPropagation();
+
+      if (isClick === 1) isClick = 0;
+      else if (isClick === 0) isClick = 1;
+
+      if (isClick === 1) {
+        myCalendar.focus();
+      }
+    });
+
+    $(myCalendar).on("click", function (e) {
+      e.stopPropagation();
+      isClick = 1;
+    });
+
+    $(".daterangepicker").on("click", function (e) {
+      e.stopPropagation();
+    });
+  } catch (er) {
+    console.log(er);
+  }
+  /*[ Select 2 Config ]
+      ===========================================================*/
+
+  try {
+    var selectSimple = $(".js-select-simple");
+
+    selectSimple.each(function () {
+      var that = $(this);
+      var selectBox = that.find("select");
+      var selectDropdown = that.find(".select-dropdown");
+      selectBox.select2({
+        dropdownParent: selectDropdown,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+})(jQuery);
+// لعرض وإخفاء كلمة المرور
+// const togglePassword = document.querySelector("#togglePassword");
+// const password = document.querySelector("#id_password");
+
+// togglePassword.addEventListener("click", function (e) {
+//   // toggle the type attribute
+//   const type =
+//     password.getAttribute("type") === "password" ? "text" : "password";
+//   password.setAttribute("type", type);
+//   // toggle the eye slash icon
+//   this.classList.toggle("far fa-eye");
+// });
